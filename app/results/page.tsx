9064965eb4178089c13d8117e622795583d98d70
iaavas <baralaavas@gmail.com>
2024-01-20 17:54:52 +0545

    Bookings problem

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,21 +1,35 @@
+"use client";
 import useBlockchain from "@/services/useBlockchain";
-import UserDetails from "../components/user/UserDetail";
-import { useEffect, useState } from "react";
+
+import { useEffect, useLayoutEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 export default function User() {
   const { getAllBookings } = useBlockchain();
   const [isLoading, setIsLoading] = useState(false);
-  useEffect(function () {
-    async function fetchBookings() {
-      try {
-        setIsLoading(true);
-        const data = await getAllBookings();
-      } catch (err) {
-        toast.error("error occured whle fetching");
-      }
-    }
-    fetchBookings();
+  const [bookings, setBookings] = useState([]);
+
+  const fsdj = async () => {
+    console.log("calling");
+    await getAllBookings().then((res) => console.log(res));
+  };
+  useLayoutEffect(() => {
+    fsdj();
   }, []);
-  return <></>;
+  // useEffect(
+  //   function () {
+  //     async function fetchBookings() {
+  //       try {
+  //         setIsLoading(true);
+  //         const data = await getAllBookings();
+  //         setBookings(data);
+  //       } catch (err) {
+  //         toast.error("error occured whle fetching");
+  //       }
+  //     }
+  //     fetchBookings();
+  //   },
+  //   [getAllBookings]
+  // );
+  return <div>{bookings}</div>;
 }
